c988213810140dd702ff7512a0098253d8c756b6
abigael mwelu <abigael.mutuku@student.moringaschool.com>
2023-10-26 09:56:47 +0300

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <Link to="/" className="logo">
+        Agricultural
+        </Link>
+            <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/experts">Experts</Link>
+            <Link to="/communities">Communities</Link>
+            </div>
+        <div className="user-links">
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        </div>
+   </nav>
+  )
+}
+
+export default Navbar
