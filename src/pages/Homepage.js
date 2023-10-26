b6148dc365ec4968ca5641ca110a736185f6cef0
Kamente <justinm48000@gmail.com>
2023-10-26 13:11:25 +0300

    cards addition

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,62 @@
-import React from "react";
-import './Homepage.css'
+import React, { useState } from "react";
+import './Homepage.css';
+import Cultural from '../images/Cultural.png';
+import { Link } from "react-router-dom";
+
 function HomePage() {
+    const [flipped, setFlipped] = useState(false);
+
+    const handleCardFlip = () => {
+        setFlipped(!flipped);
+    }
+
     return (
         <div>
-            <p>Hello There</p>
+            <nav className="custom-nav">
+                <div className="content1">
+                    <img className="custom-image" src={Cultural} alt="logo" />
+                    <input className="custom-input" type="text" placeholder="Search" />
+                    <button className="custom-search">Search</button>
+                    <li>
+                        <button className="login"><Link to="/signup">Login/Signup</Link></button>
+                    </li>
+                </div>
+                <div className="content2">
+                    <ul className="list2"> {/* Changed li to ul */}
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Blog/Post List</Link></li>
+                        <li><Link to="/">Profile</Link></li>
+                        <li><Link to="/">Create Blog/Post</Link></li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="cards-section">
+                <div className={`card ${flipped ? 'flipped' : ''}`} onClick={handleCardFlip}>
+                    <div className="card-front">
+                        <h2>Title</h2>
+                        <h2>Author</h2>
+                        <button>Read More</button>
+                    </div>
+                    <div className="card-back">
+                        <p>Description</p>
+                        <button>Close</button>
+                    </div>
+                </div>
+                <div className={`card ${flipped ? 'flipped' : ''}`} onClick={handleCardFlip}>
+                    <div className="card-front">
+                        <h2>Title</h2>
+                        <h2>Author</h2>
+                        <button>Read More</button>
+                    </div>
+                    <div className="card-back">
+                        <p>Description</p>
+                        <button>Close</button>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
+
 export default HomePage;
