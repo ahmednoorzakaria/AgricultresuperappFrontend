cf00c9fc1d90931ebb3636062d955f5191e16ac3
ahmed <ahmednoorzakaria680@gmail.com>
2023-10-30 12:13:46 +0300

    finished all changes

---
+++ src/components/NavBar/VideoBackground.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideoBackground = () => {
+  return (
+    <video autoPlay loop muted playsInline>
+      <source src="https://www.dropbox.com/scl/fi/iat0la8y1fi6gbjloxzxn/video.mp4?rlkey=w5e60tg0bydlc4zdb7w0eqj6l&dl=1" type="video/mp4" />
+    </video>
+  );
+};
+
+export default VideoBackground;
