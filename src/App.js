1236a397f8284a3a2e53927785dffa4761fca8b5
Kamente <justinm48000@gmail.com>
2023-10-31 21:47:49 +0300

    routes update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import SignUpPage from './components/SignUpPage';
-import UserProfileForm from './components/UserProfileForm';
-import HomePage from './components/Homepage';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Page from './components/Landing page/landingPage';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/profile" element={<UserProfileForm />} />
-          <Route path="/homepage" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Page />} />
+    </Routes>
+  </Router>
   );
 }
 
