5de862deda413706cd98c7b36175b80f9e004b04
BrwnBoy <brianmm665@gmial.com>
2023-11-01 11:04:09 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Page from './components/Landing page/landingPage';
+import Login from './components/Log-In/LogInPage';
+import Signup from './components/Log-In/SignUpPage'
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<Page />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   </Router>
   );
