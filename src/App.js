dc2a83a05b0c2f8f9675dc4602512c0f478c2df2
ahmed <ahmednoorzakaria680@gmail.com>
2023-11-05 22:24:11 +0300

    connected the backend with frontend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Page from './components/Landing page/landingPage';
 import Nav from './components/NavBar/Nav';
@@ -7,13 +7,16 @@ import SignUp from './components/Log-In/SignUpPage';
 import BlogDetails from './components/BlogDetails/BlogDetails';
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(""); // Define loggedInUser state
+
   return (
     <Router>
       <div className="app-container">
-        <Nav />
+        <Nav loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
         <Routes>
           <Route path="/" element={<Page />} />
-          <Route path="/login" element={<LogIn />} />
+          <Route path="/login" element={<LogIn loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />} />
+          {/* Pass setLoggedInUser as a prop to LogIn */}
           <Route path="/signup" element={<SignUp />} />
           <Route path="/blog/:id" element={<BlogDetails />} />
         </Routes>
